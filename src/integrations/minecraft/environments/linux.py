aea464c55a1e80449e383688b0ee49c5e7e22aad
itzzsauravp <social.saurav2003@gmail.com>
2026-02-15 18:23:41 +0545

    feat: environment setup for linux(debian)

--- src/integrations/minecraft/environments/linux.py
+++ src/integrations/minecraft/environments/linux.py
@@ -1,2 +1,37 @@
+from ..preflight_checks import system_has_java
+import subprocess
+
+def setup_java():
+    if system_has_java():
+        print("Java is already installed.")
+        return
+
+    print("Java is not installed. Installing OpenJDK 17...")
+    
+    try:
+        subprocess.run(
+            ["sudo", "apt", "update"],
+            check=True
+        )
+
+        subprocess.run(
+            ["sudo", "apt", "install", "-y", "openjdk-17-jdk"],
+            check=True
+        )
+
+        print("Java installation complete!")
+
+        
+        result = subprocess.run(["java", "-version"], capture_output=True, text=True)
+        print(result.stdout)
+        print(result.stderr)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing Java: {e}")
+        exit(1)
+
+
 def setup_linux_environment():
     print("Setting up Linux environment for Minecraft server...")
+    setup_java()
+    print("Environment setup complete!")
